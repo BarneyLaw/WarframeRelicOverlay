3cafef9ed843e3076d0428a988301ed2895391aa
Leifsen <leifsenlaw993@gmail.com>
2026-05-21 20:25:06 +0800

    Added converter to change naming of items to url version

---
+++ src/Infrastructure/Market/MarketSlugConverter.cs
@@ -0,0 +1,24 @@
+namespace WarframeRelicOverlay.Infrastructure.Market;
+
+public sealed class MarketSlugConverter
+{
+    /// <summary>
+    /// Converts a market item name to a slug used in the Warframe Market API.
+    /// E.g. "cobra & crane prime hilt" -> "cobra_and_crane_prime_hilt"
+    /// Handles special cases like "Prime Access" and "Bundle".
+    /// </summary>
+    public static string ToSlug(string itemName)
+    {
+        // Handle special cases first
+        if (itemName.Contains("&", StringComparison.OrdinalIgnoreCase))
+            return itemName.Replace("&", "and").Replace(" ", "_").ToLower().Trim();
+
+                // 2x forma will be hardcoded since it does not follow any patterns
+        // and it is not tradeable
+        if (itemName.Equals("2x Forma Blueprint", StringComparison.OrdinalIgnoreCase))
+            return "forma_blueprint";    
+
+        // General case: replace spaces with underscores and lowercase
+        return itemName.Replace(" ", "_").ToLower().Trim();
+    }
+}
---
+++ tests/Infrastructure.Tests/MarketSlugConverterTest.cs
@@ -0,0 +1,43 @@
+﻿namespace Infrastructure.Tests;
+
+using FluentAssertions;
+using Xunit;
+
+public class MarketSlugConverterTest
+{
+    [Fact]
+    public void Space_Replaced_With_Underscore()
+    {
+        string itemName1 = "Ash Prime Chassis Blueprint";
+        string expected1 = "ash_prime_chassis_blueprint";
+
+        string itemName2 = "Corinth Prime Blueprint";
+        string expected2 = "corinth_prime_blueprint";
+
+        string itemName3 = "Forma Blueprint";
+        string expected3 = "forma_blueprint";
+
+        string itemName4 = "2x Forma Blueprint";
+        string expected4 = "forma_blueprint";
+
+        string result1 = MarketSlugConverter.ToSlug(itemName1);
+        string result2 = MarketSlugConverter.ToSlug(itemName2);
+        string result3 = MarketSlugConverter.ToSlug(itemName3);
+
+        result1.Should().Be(expected1);
+        result2.Should().Be(expected2);
+        result3.Should().Be(expected3);
+    }
+
+    [Fact]
+
+    public void Ampersand_Replaced_With_And()
+    {
+        string itemName = "Cobra & Crane Prime Hilt";
+        string expected = "cobra_and_crane_prime_hilt";
+
+        string result = MarketSlugConverter.ToSlug(itemName);
+
+        result.Should().Be(expected);
+    }
+}
